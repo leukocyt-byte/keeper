45dce5d2ecfc1956f723cdcf7f334d49b93d8e88
Justyna <justynka.takk@gmail.com>
2021-04-08 11:21:21 +0200

    home&about pages added

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>About this App </h1>
+      <p className="my-1">
+        {' '}
+        This is full stack React app for keeping contacts.
+      </p>
+      <p className="bg-dark p">Versions 1.0.0</p>
+    </div>
+  );
+};
+
+export default Home;
