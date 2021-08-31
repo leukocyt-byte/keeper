ca0700dddacc87e8f9bc27233a66b88805ab533a
Justyna <justynka.takk@gmail.com>
2021-08-31 11:24:35 +0200

    private route

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -6,12 +6,16 @@ import AuthContext from '../../context/auth/authContext';
 const Navbar = ({ title, icon }) => {
   const authContext = useContext(AuthContext);
 
-  const { isAuthenticated, logut, user } = authContext;
+  const { isAuthenticated, logout, user } = authContext;
+
+  const onLogout = () => {
+    logout();
+  }
   const authLinks = (
     <Fragment>
       <li>Hello {user && user.name} </li>
       <li>
-        <a href="#!">
+        <a onClick={onLogout} href="#!">
           <i className="fas fa-sign-out-alt"></i>
           <span className="hide-sm">Logout</span>
         </a>
